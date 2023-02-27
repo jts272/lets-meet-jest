a01376c96cf6bdf0160e60cb419f057721ae269a
jts272 <jts272@outlook.com>
2023-02-27 14:31:08 +0000

    test: add test with full html file in mock dom

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -12,9 +12,12 @@ const buttonClick = require("../button");
 // jsdom mock dom creation. This is needed because we are not loading an actual
 // HTML page.
 
-beforeEach(() => {
-  document.body.innerHTML = "<p id='par'></p>";
-});
+// This first beforeEach method is commented out so that the full DOM mock can
+// be used below the dividing line.
+
+// beforeEach(() => {
+//   document.body.innerHTML = "<p id='par'></p>";
+// });
 
 // ACTUAL TESTS:
 
@@ -28,4 +31,27 @@ describe("DOM Tests", () => {
     // Checks the content of the innerHTML of the given element.
     expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
   });
+  // ADDITIONAL TEST TO CHECK THE DOM IS BEING READ CORRECTLY:
+  test("h1 should exist", () => {
+    expect(document.getElementsByTagName("h1").length).toBe(1);
+  });
+});
+
+//----------------------------------------------------------------------------//
+
+// LOADING THE ENTIRE HTML PAGE AND ATTACHING TO THE MOCK DOM:
+
+beforeEach(() => {
+  // Use node.js' built-in file system handling module for opening, reading and
+  // writing files:
+  let fs = require("fs");
+  // Use fs to read the contents of our HTML file and specify encoding to return
+  // a string:
+  // (File path not specified because by default, the tests run from node's root
+  // dir, which is where our index.html file is.)
+  let fileContents = fs.readFileSync("index.html", "utf-8");
+  // Recommended method of attaching this file to the mock DOM:
+  document.open();
+  document.write(fileContents);
+  document.close();
 });
