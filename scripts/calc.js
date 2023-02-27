8bb783f0d2fe81e805fafc65939888436f4a9bd6
jts272 <jts272@outlook.com>
2023-02-27 12:51:47 +0000

    test: include additional tests and test ideas
    Jest VS Code extension installed for rapid test feedback.

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,9 +1,21 @@
 // Write minimum code required to pass the addition function:
 
-function addition() {
-  return 42;
-}
+// function addition() {
+//   return 42;
+// }
 
 // Export the function so the test can read it:
+
+// Write minimum code to pass first and second test:
+
+// function addition(n1, n2) {
+//   return n1 + n2;
+// }
+
+// Try arrow function:
+
+const addition = (n1, n2) => n1 + n2;
+
+// NOTE: Arrow functions must be declared before `module.exports`
 
 module.exports = addition;
--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -14,7 +14,41 @@ describe("Calculator", () => {
       // EXPECTATION:
       expect(addition(20, 22)).toBe(42);
     });
+    // TEST 2:
+    test("should return 73 for 42 + 31", () => {
+      // EXPECTATION:
+      expect(addition(42, 31)).toBe(73);
+    });
+    // TEST 3 (idea):
+    test("should return error for 42 + 'donkey'", () => {
+      //  EXPECTATION:
+      expect(addition(42, "donkey")).toBe(Error);
+    });
+    // ANSWERS FOR FURTHER TEST SCENARIOS:
+    // 1. No params
+    // 2. Missing param
+    // 3. String as one or both params
+    // 4. Null as one or both params
+    // 5. Undefined as a param
+    // 6. Use floats
+    // 7. Use negatives
+    // TEST (idea 6):
+    test("should return 3.5 for 1.5 + 2", () => {
+      //  EXPECTATION:
+      expect(addition(1.5, 2)).toBe(3.5);
+    });
+    // TEST (idea 7):
+    test("should return -9 for -3 + -6", () => {
+      //  EXPECTATION:
+      expect(addition(-3, -6)).toBe(-9);
+    });
+    // TEST (idea 5):
+    test("should return null for null + null", () => {
+      //  EXPECTATION:
+      expect(addition(null, null)).toBe(null);
+    });
   });
+  // ---------------------------------------------------------------- //
   describe("Subtraction function", () => {});
   describe("Multiplication function", () => {});
   describe("Division function", () => {});
